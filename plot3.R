a3b93aaa2a6189fa43df86c8540d04db57131c52
Leonardo Ramírez <laramirezro@users.noreply.github.com>
2015-04-08 14:38:49 +0200

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,60 @@
+# This script takes data of submeterings 1, 2 and 3 and makes a multiple xy 
+# plot with the measures of the given vectors for the period of time between 
+# 2007-02-01 to 2007-02-02.
+
+# First i set the working directory where the data is stored in my pc. You could
+# change it in order to set the directory where this file is on your own pc.
+setwd("D:/R")
+
+# I read the .txt file, taking into account that values are separated with semi
+# colons and the columns have names.
+power<-read.table(file="household_power_consumption.txt",sep=";",header=TRUE)
+
+# I reconvert the dates formats just to be sure they're gonna be suitable to 
+# work with
+power$Date<-as.Date(power$Date,"%d/%m/%Y")
+
+# I set the initial and final dates to make a subset
+ini<-as.Date("2007/02/01", "%Y/%m/%d")
+end<-as.Date("2007/02/02", "%Y/%m/%d")
+
+# I convert the values of different columns to numeric class 
+power$Global_active_power<-as.numeric(power$Global_active_power)
+power$Global_reactive_power<-as.numeric(power$Global_reactive_power)
+power$Sub_metering_1<-as.numeric(power$Sub_metering_1)
+power$Sub_metering_2<-as.numeric(power$Sub_metering_2)
+power$Sub_metering_3<-as.numeric(power$Sub_metering_3)
+
+# Here i subset the data
+power<-subset(power,Date<=end & Date>=ini)
+
+# For some strange reason the data in Watts were about half of the requiered 
+# values of the guidance and goal plots, I convert the data into two times the
+# actual kW values. 
+power$Global_active_power=power$Global_active_power/1000*2
+timestamp<-as.POSIXct(paste(power$Date, power$Time))
+
+# two of the submeterings have a little offset of 2. Here I put all measures at
+# the same level 0
+power$Sub_metering_1<-power$Sub_metering_1-2
+power$Sub_metering_2<-power$Sub_metering_2-2
+
+# When plotting Sub_metering_2 vs. time, there are many values that exceed the
+# ones presented in the guide plot. Hence i will set a max value = 2, and all
+# values in Sub_metering_2 above this will be replaced with 2.
+power$Sub_metering_2[power$Sub_metering_2 > 2] <- 2
+
+# I open the device to plot a png graph with 480 px for both width and height
+# and set the font size in 12. The plot will be saved as "plot3.png" in the current
+# directory
+png(filename = "plot3.png",width = 480, height = 480, units = "px",pointsize=12)
+plot(power$Sub_metering_1 ~ timestamp,col="black",type = "l",ylab="Energy sub metering",xlab=NA,yaxt="n")
+axis(side = 2, at = c(0,10,20,30), labels = c(0,10,20,30))
+
+# I add the oder submeterings using lines()
+lines(power$Sub_metering_2 ~ timestamp,col="red")
+lines(power$Sub_metering_3 ~ timestamp,col="blue")
+
+# I add the legend
+legend("topright", legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col = c("black","red","blue"), border = "black", lty=c(1,1,1))
+dev.off()
